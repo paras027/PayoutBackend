314d46b043f2acd28ba5be53c0f6a03ed48fccc1
paras027 <parasbhardwaj.ps@gmail.com>
2026-04-29 04:41:07 +0530

    fixed db url detection issue

--- PlaytoPayout/settings.py
+++ PlaytoPayout/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_sdmqchcymqfsxf%qq)2+xfql&bf!deif#kzra!m3dhv4c%a*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
